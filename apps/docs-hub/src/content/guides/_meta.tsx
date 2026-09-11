34818b83d7c0e7798619e30467eed81430823c47
Alex <argeares97@gmail.com>
2026-09-11 17:36:27 +0300

    fix: added siwx guide

--- apps/docs-hub/src/content/guides/_meta.tsx
+++ apps/docs-hub/src/content/guides/_meta.tsx
@@ -3,5 +3,6 @@ export default {
   '--': {
     type: 'separator',
   },
+  'multi-chain-auth-siwx-caip122': 'Multi-Chain Auth with SIWX',
   'why-web3-transaction-state-is-broken': 'Why Web3 Tx State is Broken',
 };
